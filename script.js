e8afca5b4c01bbffe95f00bdc604c9bd4eb524b1
Ana Ferreira <an47erreira@gmail.com>
2025-05-03 19:07:46 -0300

    new version

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-document.body.style.zoom = "67%"
-
-
-
-
 // Função para abrir o popup
 function openPopup(popupId) {
     const popup = document.getElementById(popupId);
     const pageContent = document.getElementById('page-content');
-    
+
     popup.style.display = 'block';
     pageContent.classList.add('blur');  // Adiciona o desfoque ao conteúdo
 
@@ -23,7 +18,7 @@ function openPopup(popupId) {
 function closePopup(popupId) {
     const popup = document.getElementById(popupId);
     const pageContent = document.getElementById('page-content');
-    
+
     popup.style.display = 'none';
     pageContent.classList.remove('blur');  // Remove o desfoque do conteúdo
 }
