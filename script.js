63b885fa303d1ac12013c2be513b9a2d93a6ba00
Ana Ferreira <an47erreira@gmail.com>
2025-05-25 17:57:38 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Função para abrir o popup
+function openPopup(popupId) {
+    const popup = document.getElementById(popupId);
+    const pageContent = document.getElementById('page-content');
+
+    popup.style.display = 'block';
+
+    // Fechar o popup clicando fora dele
+    window.onclick = function (event) {
+        if (event.target == popup) {
+            closePopup(popupId);
+        }
+    }
+}
+
+// Função para fechar o popup
+function closePopup(popupId) {
+    const popup = document.getElementById(popupId);
+    const pageContent = document.getElementById('page-content');
+
+    popup.style.display = 'none';
+}
+
